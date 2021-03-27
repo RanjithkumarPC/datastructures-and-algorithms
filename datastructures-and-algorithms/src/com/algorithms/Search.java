dfced4481c13094eb1c6a4f49edbc1eb9378af7b
RanjithkumarPC <81403335+RanjithkumarPC@users.noreply.github.com>
2021-03-27 18:05:33 +0530

    Initial Commit

---
+++ datastructures-and-algorithms/src/com/algorithms/Search.java
@@ -0,0 +1,20 @@
+package com.algorithms;
+
+public class Search {
+
+	public static void main(String[] args) {
+		int input[] = {1,4,7,2,3};
+		int number = 10;
+		System.out.println(findSearchedIndex(input, number));
+	}
+	
+	public static int findSearchedIndex(int[] input, int number) {
+		for(int i = 0; i < input.length; i++) { // n
+			if(input[i] == number) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
